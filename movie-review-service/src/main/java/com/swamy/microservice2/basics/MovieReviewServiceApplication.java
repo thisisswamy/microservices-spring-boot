f39653dcc6f0e688b9264dd1d9e1c9a0e8fe2e3e
Swamy <thudumswamy786@gmail.com>
2023-04-09 02:46:58 +0530

    added authentication

--- movie-review-service/src/main/java/com/swamy/microservice2/basics/MovieReviewServiceApplication.java
+++ movie-review-service/src/main/java/com/swamy/microservice2/basics/MovieReviewServiceApplication.java
@@ -3,9 +3,11 @@ package com.swamy.microservice2.basics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class MovieReviewServiceApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/com/swamy/microservice/basics/configuration/MySecurityConfig.java
+++ user-service/src/main/java/com/swamy/microservice/basics/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.swamy.microservice.basics.configuration;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,27 +12,36 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.swamy.microservice.basics.JWT.JwtFilter;
+import com.swamy.microservice.basics.exceptions.JwtEntryPointException;
+
 import com.swamy.microservice.basics.repos.UserRepo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @EnableWebSecurity
 @Configuration
 public class MySecurityConfig {
-	
-	private static final String[] SECURE_URLS= {"/all", "/{userName}"};
-	private static final String[] PUBLIC_URLS= {"/register",};
+
+	private static final String[] PUBLIC_URLS = {"/register", "/auth/login"};
 	
 	@Autowired
 	private UserRepo userRepo;
@@ -46,6 +56,11 @@ public class MySecurityConfig {
 	private JwtFilter jwtFilter;
 	
 	
+	@Autowired
+	private JwtEntryPointException jwtEntryPointException;
+
+	
+	
 	@Bean  //Exposing some routes to publicly
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().requestMatchers(PUBLIC_URLS);
@@ -53,8 +68,11 @@ public class MySecurityConfig {
 	
 	 @Bean
 	 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated());
-	        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+	        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+	        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	        .and()
+	        .exceptionHandling().authenticationEntryPoint(jwtEntryPointException);
+	        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);        
 	        return http.build();
 	 }
 	 
@@ -66,6 +84,11 @@ public class MySecurityConfig {
 		 return authProvider;
 	 }
 	 
-		
-
+	 
+	 @Bean
+	 public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception { 
+		 return configuration.getAuthenticationManager();
+	 }
+	 
+	
 }
