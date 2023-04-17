7d1f5cc37ee847771645d5c6f9784f4aac14bd9e
Swamy <thudumswamy786@gmail.com>
2023-04-17 21:29:54 +0530

    implemented file storing system

--- movie-review-service/src/main/java/com/swamy/microservice2/basics/docs/ReviewFormImage.java
+++ movie-review-service/src/main/java/com/swamy/microservice2/basics/docs/ReviewFormImage.java
@@ -15,24 +15,20 @@ public class ReviewFormImage {
 	private String imageName;
 	private long imageSize;
 	private String imageType;
-	private byte[] imageBytes;
 	
 	
-	public ReviewFormImage(String imageName, long imageSize, String imageType, byte[] imageBytes) {
+	public String getImageName() {
+		return imageName;
+	}
+	
+	public ReviewFormImage() {
 		super();
+	}
+
+	public ReviewFormImage(String imageName, long imageSize, String imageType) {
 		this.imageName = imageName;
 		this.imageSize = imageSize;
 		this.imageType = imageType;
-		this.imageBytes = imageBytes;
-	}
-	public String getImageType() {
-		return imageType;
-	}
-	public void setImageType(String imageType) {
-		this.imageType = imageType;
-	}
-	public String getImageName() {
-		return imageName;
 	}
 	public void setImageName(String imageName) {
 		this.imageName = imageName;
@@ -43,20 +39,14 @@ public class ReviewFormImage {
 	public void setImageSize(long imageSize) {
 		this.imageSize = imageSize;
 	}
-	public byte[] getImageBytes() {
-		return imageBytes;
+	public String getImageType() {
+		return imageType;
 	}
-	public void setImageBytes(byte[] imageBytes) {
-		this.imageBytes = imageBytes;
+	public void setImageType(String imageType) {
+		this.imageType = imageType;
 	}
-	public ReviewFormImage() {
-		super();
-	}
-	@Override
-	public String toString() {
-		return "ReviewFormImage [imageName=" + imageName + ", imageSize=" + imageSize + ", imageBytes="
-				+ Arrays.toString(imageBytes) + "]";
-	}
+	
+	
 	
 	
 	
